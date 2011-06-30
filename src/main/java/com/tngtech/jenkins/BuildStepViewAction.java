6933f4487d9240c0814c3021919ef0aa46f98434
Stefan Wolf <wolfs@fs.tum.de>
2011-06-30 19:50:45 +0200

    Initial checkin

---
+++ src/main/java/com/tngtech/jenkins/BuildStepViewAction.java
@@ -0,0 +1,73 @@
+package com.tngtech.jenkins;
+
+import hudson.Util;
+import hudson.model.Action;
+import hudson.model.AbstractProject;
+import hudson.model.Hudson;
+import hudson.plugins.parameterizedtrigger.TriggerBuilder;
+import hudson.tasks.Builder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BuildStepViewAction implements Action {
+
+    public class BuilderInfo {
+        public String jobName;
+        public List<AbstractProject> projects;
+    }
+
+    private final List<Builder> builders;
+
+    public BuildStepViewAction(List<Builder> builders) {
+        this.builders = builders;
+    }
+
+    public String getIconFileName() {
+        return "monitor.gif";
+    }
+
+    public String getDisplayName() {
+        return "Builds running";
+    }
+
+    public String getUrlName() {
+        return "build-step-view";
+    }
+
+    public List<BuilderInfo> getBuilderInfos() {
+        ArrayList<BuilderInfo> builderInfos = new ArrayList<BuilderInfo>();
+        for (Builder builder : builders) {
+            BuilderInfo builderInfo = new BuilderInfo();
+            if (builder instanceof TriggerBuilder) {
+                TriggerBuilder tBuilder = (TriggerBuilder) builder;
+
+                builderInfo.jobName = tBuilder.getConfigs().iterator().next().getProjects();
+                builderInfo.projects = getProjectsForNames(builderInfo.jobName);
+            }
+            builderInfos.add(builderInfo);
+        }
+        return builderInfos;
+    }
+
+    private List<AbstractProject> getProjectsForNames(String jobNames) {
+        ArrayList<AbstractProject> projects = new ArrayList<AbstractProject>();
+        StringTokenizer tokens = new StringTokenizer(Util.fixNull(jobNames),",");
+        while(tokens.hasMoreTokens()) {
+            projects.add(getProjectForName(tokens.nextToken().trim()));
+        }
+        return projects;
+    }
+
+    private AbstractProject getProjectForName(String jobName) {
+        List<AbstractProject> items = Hudson.getInstance().getItems(AbstractProject.class);
+        for (AbstractProject abstractProject : items) {
+            if (jobName.equals(abstractProject.getName())) {
+                return abstractProject;
+            }
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/com/tngtech/jenkins/BuildStepViewBuildWrapper.java
@@ -0,0 +1,48 @@
+package com.tngtech.jenkins;
+
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.FreeStyleBuild;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildWrapper;
+import hudson.tasks.BuildWrapperDescriptor;
+import hudson.tasks.Builder;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class BuildStepViewBuildWrapper extends BuildWrapper {
+    @Override
+    public Environment setUp(AbstractBuild build, Launcher launcher,
+            BuildListener listener) throws IOException, InterruptedException {
+        FreeStyleBuild fBuild = (FreeStyleBuild) build;
+        List<Builder> builders = fBuild.getProject().getBuilders();
+        build.addAction(new BuildStepViewAction(builders));
+        return new Environment() {
+        };
+    }
+
+    @DataBoundConstructor
+    public BuildStepViewBuildWrapper() {
+    }
+
+    @Extension
+    public static class BuildStepViewBuildWrapperDescriptor extends
+            BuildWrapperDescriptor {
+
+        @Override
+        public boolean isApplicable(AbstractProject<?, ?> item) {
+            return true;
+        }
+
+        @Override
+        public String getDisplayName() {
+            return "View Build Steps";
+        }
+
+    }
+}
